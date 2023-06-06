7efd106686a66cd7769a5305878b780cd2d5e795
portravedesign <66311356+portravedesign@users.noreply.github.com>
2023-06-06 13:00:48 +0530

    video added
    video added

--- js/app.js
+++ js/app.js
@@ -4,14 +4,14 @@ var mainSection = document.querySelector('.main_section');
 
 mainSection.style.minHeight = (windowHeight - headerHeight)  + "px";
 
-const webcamElement = document.getElementById('webcam');
-const canvasElement = document.getElementById('canvas');
-const webcam = new Webcam(webcamElement, 'user', canvasElement);
+// const webcamElement = document.getElementById('webcam');
+// const canvasElement = document.getElementById('canvas');
+// const webcam = new Webcam(webcamElement, 'user', canvasElement);
 
-webcam.start()
-   .then(result =>{
-      console.log("webcam started");
-   })
-   .catch(err => {
-       console.log(err);
-   });
+// webcam.start()
+//    .then(result =>{
+//       console.log("webcam started");
+//    })
+//    .catch(err => {
+//        console.log(err);
+//    });
