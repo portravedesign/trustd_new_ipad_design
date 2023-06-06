ad2c8456cb108175adf4bea031ad793aaa46c3dd
portravedesign <66311356+portravedesign@users.noreply.github.com>
2023-06-06 18:17:05 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,17 +1,26 @@
-var windowHeight = window.innerHeight;
-var headerHeight = document.querySelector('.header').clientHeight;
-var mainSection = document.querySelector('.main_section');
 
-mainSection.style.minHeight = (windowHeight - headerHeight)  + "px";
 
-// const webcamElement = document.getElementById('webcam');
-// const canvasElement = document.getElementById('canvas');
-// const webcam = new Webcam(webcamElement, 'user', canvasElement);
+window.addEventListener("DOMContentLoaded", function(){
+    // var windowHeight = window.innerHeight;
+    // var headerHeight = document.querySelector('.header').clientHeight;
+    // var mainSection = document.querySelector('.main_section');
+    // mainSection.style.minHeight = (windowHeight - headerHeight)  + "px";
 
-// webcam.start()
-//    .then(result =>{
-//       console.log("webcam started");
-//    })
-//    .catch(err => {
-//        console.log(err);
-//    });
+    var width = (window.innerWidth)
+    var vidContainer = document.querySelector(".video_container");
+    var overlayImage = document.querySelector(".overlay_image");
+    var overlayRest = document.querySelector(".overlay_rest");
+       var vidContainerHeight = vidContainer.clientHeight;
+       var overlayImageHeight = overlayImage.clientHeight;
+       if(width<991){
+        overlayRest.style.height = (vidContainerHeight - overlayImageHeight) + "px";
+       }
+       
+
+
+
+
+})
+
+
+
