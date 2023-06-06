6ba03fed39aab86722f0d0b6e6ba5801add21d22
portravedesign <66311356+portravedesign@users.noreply.github.com>
2023-06-06 12:05:28 +0530

    video test
    video test

--- js/app.js
+++ js/app.js
@@ -4,14 +4,3 @@ var mainSection = document.querySelector('.main_section');
 
 mainSection.style.minHeight = (windowHeight - headerHeight)  + "px";
 
-const webcamElement = document.getElementById('webcam');
-const canvasElement = document.getElementById('canvas');
-const webcam = new Webcam(webcamElement, 'user', canvasElement);
-
-webcam.start()
-   .then(result =>{
-      console.log("webcam started");
-   })
-   .catch(err => {
-       console.log(err);
-   });
