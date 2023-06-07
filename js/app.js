45a019884942e195737cd6d66b1388f2a32c6312
portravedesign <66311356+portravedesign@users.noreply.github.com>
2023-06-07 09:56:45 +0530

    fixed camera overlay
    fixed camera overlay and responsive issues.....testing pending

--- js/app.js
+++ js/app.js
@@ -1,25 +1,10 @@
 
 
 window.addEventListener("DOMContentLoaded", function(){
-    // var windowHeight = window.innerHeight;
-    // var headerHeight = document.querySelector('.header').clientHeight;
-    // var mainSection = document.querySelector('.main_section');
-    // mainSection.style.minHeight = (windowHeight - headerHeight)  + "px";
-
-    var width = (window.innerWidth)
-    var vidContainer = document.querySelector(".video_container");
-    var overlayImage = document.querySelector(".overlay_image");
-    var overlayRest = document.querySelector(".overlay_rest");
-       var vidContainerHeight = vidContainer.clientHeight;
-       var overlayImageHeight = overlayImage.clientHeight;
-       if(width<991){
-        overlayRest.style.height = (vidContainerHeight - overlayImageHeight) + "px";
-       }
-       
-
-
-
-
+    var windowHeight = window.innerHeight;
+    var headerHeight = document.querySelector('.header').clientHeight;
+    var mainSection = document.querySelector('.main_section');
+    mainSection.style.minHeight = (windowHeight - headerHeight)  + "px";
 })
 
 
